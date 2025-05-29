1fd01c59eb7e929b5fa4d22919098a8977d8ac8e
xxxavi17 <xavier.f.machado@ua.pt>
2025-05-29 15:03:36 +0100

    fix ver tabela de clientes

--- Streaming_Forms/Streaming_BD/Form3.cs
+++ Streaming_Forms/Streaming_BD/Form3.cs
@@ -1,4 +1,6 @@
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace Streaming_BD
 {
@@ -7,6 +9,28 @@ namespace Streaming_BD
         public Form3()
         {
             InitializeComponent();
+        }
+
+        protected override void OnLoad(System.EventArgs e)
+        {
+            base.OnLoad(e);
+            CarregarClientes();
+        }
+
+        private void CarregarClientes()
+        {
+            string connectionString = "Server=tcp:mednat.ieeta.pt\\SQLSERVER,8101;Database=p1g11;User Id=p1g11;Password=Theoxavi11;TrustServerCertificate=True";
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT id_cliente, nome, email, data_nascimento, sexo FROM Streaming_Cliente";
+                using (var adapter = new SqlDataAdapter(query, conn))
+                {
+                    DataTable dt = new DataTable();
+                    adapter.Fill(dt);
+                    dgvClientes.DataSource = dt;
+                }
+            }
         }
 
         // Botão: Menu Inicial
