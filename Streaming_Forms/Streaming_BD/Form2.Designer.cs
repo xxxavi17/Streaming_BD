75bf04b0e32dd73f855b15b49da633c2951af6f6
xxxavi17 <xavier.f.machado@ua.pt>
2025-05-29 14:39:26 +0100

    update forms2

--- Streaming_Forms/Streaming_BD/Form2.Designer.cs
+++ Streaming_Forms/Streaming_BD/Form2.Designer.cs
@@ -28,38 +28,23 @@ namespace Streaming_BD
         /// </summary>
         private void InitializeComponent()
         {
+            // Labels
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblDataNascimento = new System.Windows.Forms.Label();
+            this.lblSexo = new System.Windows.Forms.Label();
+            // TextBoxes
             this.txtNome = new System.Windows.Forms.TextBox();
             this.txtEmail = new System.Windows.Forms.TextBox();
             this.txtDataNascimento = new System.Windows.Forms.TextBox();
+            this.txtSexo = new System.Windows.Forms.TextBox();
+            // Buttons
             this.btnMenuInicial = new System.Windows.Forms.Button();
             this.btnAdicionar = new System.Windows.Forms.Button();
             this.btnVerListaClientes = new System.Windows.Forms.Button();
-            this.txtSexo = new System.Windows.Forms.TextBox();
+            // Título
+            this.lblTitulo = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // txtNome
-            // 
-            this.txtNome.Location = new System.Drawing.Point(50, 50);
-            this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(200, 27);
-            this.txtNome.TabIndex = 0;
-            this.txtNome.PlaceholderText = "Nome";
-            // 
-            // txtEmail
-            // 
-            this.txtEmail.Location = new System.Drawing.Point(50, 100);
-            this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(200, 27);
-            this.txtEmail.TabIndex = 1;
-            this.txtEmail.PlaceholderText = "Email";
-            // 
-            // txtDataNascimento
-            // 
-            this.txtDataNascimento.Location = new System.Drawing.Point(50, 150);
-            this.txtDataNascimento.Name = "txtDataNascimento";
-            this.txtDataNascimento.Size = new System.Drawing.Size(200, 27);
-            this.txtDataNascimento.TabIndex = 2;
-            this.txtDataNascimento.PlaceholderText = "Data de Nascimento";
             // 
             // btnMenuInicial
             // 
@@ -91,35 +76,98 @@ namespace Streaming_BD
             this.btnVerListaClientes.UseVisualStyleBackColor = true;
             this.btnVerListaClientes.Click += new System.EventHandler(this.btnVerListaClientes_Click);
             // 
-            // txtSexo
+            // lblTitulo
             // 
-            this.txtSexo.Location = new System.Drawing.Point(50, 200);
-            this.txtSexo.Name = "txtSexo";
-            this.txtSexo.Size = new System.Drawing.Size(200, 27);
-            this.txtSexo.TabIndex = 3;
-            this.txtSexo.PlaceholderText = "Sexo";
+            this.lblTitulo.Text = "Adicionar Cliente";
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point((this.ClientSize.Width - 300) / 2, 20);
+            this.lblTitulo.Size = new System.Drawing.Size(300, 40);
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Controls.Add(this.lblTitulo);
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.txtNome);
-            this.Controls.Add(this.txtEmail);
-            this.Controls.Add(this.txtDataNascimento);
             this.Controls.Add(this.btnMenuInicial);
             this.Controls.Add(this.btnAdicionar);
             this.Controls.Add(this.btnVerListaClientes);
-            this.Controls.Add(this.txtSexo);
             this.Name = "Form2";
             this.Text = "Adicionar Cliente";
             this.ResumeLayout(false);
             this.PerformLayout();
 
+            // Centralização e layout
+            int formWidth = this.ClientSize.Width > 0 ? this.ClientSize.Width : 500;
+            int startY = 80;
+            int spacingY = 60;
+            int labelHeight = 22;
+            int textboxHeight = 28;
+            int textboxWidth = 250;
+            int centerX = (formWidth - textboxWidth) / 2;
+
+            // Nome
+            this.lblNome.Text = "Nome:";
+            this.lblNome.Location = new System.Drawing.Point(centerX, startY);
+            this.lblNome.Size = new System.Drawing.Size(textboxWidth, labelHeight);
+            this.lblNome.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txtNome.Location = new System.Drawing.Point(centerX, startY + labelHeight);
+            this.txtNome.Size = new System.Drawing.Size(textboxWidth, textboxHeight);
+
+            // Email
+            this.lblEmail.Text = "Email:";
+            this.lblEmail.Location = new System.Drawing.Point(centerX, startY + spacingY);
+            this.lblEmail.Size = new System.Drawing.Size(textboxWidth, labelHeight);
+            this.lblEmail.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txtEmail.Location = new System.Drawing.Point(centerX, startY + spacingY + labelHeight);
+            this.txtEmail.Size = new System.Drawing.Size(textboxWidth, textboxHeight);
+
+            // Data de Nascimento
+            this.lblDataNascimento.Text = "Data de Nascimento:";
+            this.lblDataNascimento.Location = new System.Drawing.Point(centerX, startY + 2 * spacingY);
+            this.lblDataNascimento.Size = new System.Drawing.Size(textboxWidth, labelHeight);
+            this.lblDataNascimento.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txtDataNascimento.Location = new System.Drawing.Point(centerX, startY + 2 * spacingY + labelHeight);
+            this.txtDataNascimento.Size = new System.Drawing.Size(textboxWidth, textboxHeight);
+
+            // Sexo
+            this.lblSexo.Text = "Sexo:";
+            this.lblSexo.Location = new System.Drawing.Point(centerX, startY + 3 * spacingY);
+            this.lblSexo.Size = new System.Drawing.Size(textboxWidth, labelHeight);
+            this.lblSexo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.txtSexo.Location = new System.Drawing.Point(centerX, startY + 3 * spacingY + labelHeight);
+            this.txtSexo.Size = new System.Drawing.Size(textboxWidth, textboxHeight);
+
+            // Adiciona controles ao formulário
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblDataNascimento);
+            this.Controls.Add(this.txtDataNascimento);
+            this.Controls.Add(this.lblSexo);
+            this.Controls.Add(this.txtSexo);
+
+            // Botão Menu Inicial
+            this.btnMenuInicial = new System.Windows.Forms.Button();
+            this.btnMenuInicial.Text = "Menu Inicial";
+            this.btnMenuInicial.Size = new System.Drawing.Size(120, 35);
+            this.btnMenuInicial.Location = new System.Drawing.Point((this.ClientSize.Width - 120) / 2, 350);
+            this.btnMenuInicial.Name = "btnMenuInicial";
+            this.btnMenuInicial.TabIndex = 100;
+            this.btnMenuInicial.UseVisualStyleBackColor = true;
+            this.btnMenuInicial.Click += new System.EventHandler(this.btnMenuInicial_Click);
+            this.Controls.Add(this.btnMenuInicial);
         }
 
         #endregion
 
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblDataNascimento;
+        private System.Windows.Forms.Label lblSexo;
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.TextBox txtDataNascimento;
@@ -127,5 +175,6 @@ namespace Streaming_BD
         private System.Windows.Forms.Button btnAdicionar;
         private System.Windows.Forms.Button btnVerListaClientes;
         private System.Windows.Forms.TextBox txtSexo;
+        private System.Windows.Forms.Label lblTitulo;
     }
 }
